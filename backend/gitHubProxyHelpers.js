4a1eebc93fc93baad65a8780ded3f218d41e210f
dstevens <david.stevens@res-group.com>
2018-06-19 17:31:43 +0200

    Fix repos with more than one page of requests
    Reinstating code removed in commit 706ffa3906b6cd8cdbb541fc7a2cb009b0cfa59f

--- backend/gitHubProxyHelpers.js
+++ backend/gitHubProxyHelpers.js
@@ -2,6 +2,18 @@ const config = require('../config');
 const url = require('url');
 
 const GITHUB_API_ROOT = 'https://api.github.com';
+const GITHUB_API_ROOT_REGEX = /https\:\/\/api\.github\.com/g;
+
+const getlocalAppRootUrl = request => {
+  return process.env.NODE_ENV == 'production'
+    ? url.format({
+        protocol: 'https', // request.protocol returns http for now since the node server itself is only using http. However the api is used over https thanks to azure / IIS
+        host: request.hostname,
+        // port: request.port,
+        pathname: ''
+      })
+    : 'https://localhost:3000'; // hard coded value in development because the request came through the webpack dev server on a different port and via https.
+};
 
 const getProxyRequestOptions = url => ({
   url: GITHUB_API_ROOT + url.replace('/api', ''),
@@ -12,6 +24,15 @@ const getProxyRequestOptions = url => ({
   }
 });
 
+const rewriteResponseHeaders = (request, response) => {
+  if (response.headers.link) {
+    response.headers.link = response.headers.link.replace(GITHUB_API_ROOT_REGEX, getlocalAppRootUrl(request) + '/api');
+    console.log(`Request App: Rewritten response.headers.link: ${JSON.stringify(response.headers.link)}`);
+  } else {
+    console.log(`Request App: response.headers.link falsy, nothing to rewrite`);
+  }
+};
+
 const genericErrorHandler = (error, response, body) => {
   if (error) {
     if (error.code === 'ECONNREFUSED') {
@@ -19,6 +40,7 @@ const genericErrorHandler = (error, response, body) => {
     } else if (error.code === 'ETIMEDOUT') {
       console.error('Connection timed out');
     } else {
+      console.error(error);
       throw error;
     }
   }
@@ -26,5 +48,6 @@ const genericErrorHandler = (error, response, body) => {
 
 module.exports = {
   getProxyRequestOptions,
-  genericErrorHandler
+  genericErrorHandler,
+  rewriteResponseHeaders
 };
