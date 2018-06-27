9c18bb4a8065dbb09702fd9d56d329858da5d79b
dstevens <david.stevens@res-group.com>
2018-06-27 11:47:23 +0200

    Explicitly install react-scripts but in the client folder

--- deploy.cmd
+++ deploy.cmd
@@ -100,11 +100,12 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install react-scripts
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
   pushd "%DEPLOYMENT_TARGET%\client"
+  call :ExecuteCmd !NPM_CMD! install react-scripts
+  IF !ERRORLEVEL! NEQ 0 goto error
   call :ExecuteCmd !NPM_CMD! run build
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
